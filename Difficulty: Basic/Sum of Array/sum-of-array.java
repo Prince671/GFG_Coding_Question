30ef876dd6c21da6792feb9d5c7771ae075df751
Prince soni <157681120+Prince671@users.noreply.github.com>
2026-08-03 18:19:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Array/sum-of-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int arraySum(int arr[]) {
+        // code here
+        int sum=0;
+        for(int val:arr){
+            sum+=val;
+        }
+        return sum;
+    }
+}
